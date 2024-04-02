7ca6212f92847d247cad15720e93322d7215192c
John Wilger <john@johnwilger.com>
2024-04-01 18:41:30 -0700

    update to astrovim v4 config

--- lua/community.lua
+++ lua/community.lua
@@ -1,12 +1,11 @@
+-- AstroCommunity: import any community modules here
+-- We import this file in `lazy_setup.lua` before the `plugins/` folder.
+-- This guarantees that the specs are processed before any user plugins.
+
+---@type LazySpec
 return {
-  -- Add the community repository of plugin specifications
   "AstroNvim/astrocommunity",
-  -- example of imporing a plugin, comment out to use it or add your own
-  -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
-
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
-  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
-
+  { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.colorscheme.tokyonight-nvim" },
   { import = "astrocommunity.git.neogit" },
   { import = "astrocommunity.diagnostics.trouble-nvim" },
@@ -16,5 +15,7 @@ return {
   { import = "astrocommunity.git.diffview-nvim" },
   { import = "astrocommunity.git.git-blame-nvim" },
   { import = "astrocommunity.split-and-window.windows-nvim" },
-  { import = "astrocommunity.motion.vim-matchup" }
+  { import = "astrocommunity.motion.vim-matchup" },
+  { import = "astrocommunity.recipes.heirline-nvchad-statusline" },
+  -- import/override with your plugins folder
 }
--- plugins/toggleterm.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "akinsho/toggleterm.nvim",
-  config = function()
-    require("toggleterm").setup({
-      open_mapping = [[<c-\>]],
-      terminal_mappings = true
-    })
-  end
-}
